fd4bd8393c401ed27826d32f492d5219cccd3b2e
Sergio <serdzo@yahoo.com>
2022-05-23 16:59:50 +0500

    Updates

---
+++ precache-manifest.3bb5b3b0cfcd3e88c34ff04f6cdc8908.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b07439388c85037f42157397059fbae",
+    "url": "/theserdzo/2MintMeBitch/index.html"
+  },
+  {
+    "revision": "4601c026681ae3e1de2b",
+    "url": "/theserdzo/2MintMeBitch/static/css/main.bdc634b4.chunk.css"
+  },
+  {
+    "revision": "d93e98511dfeb646f2a1",
+    "url": "/theserdzo/2MintMeBitch/static/js/2.ce74d095.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/theserdzo/2MintMeBitch/static/js/2.ce74d095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4601c026681ae3e1de2b",
+    "url": "/theserdzo/2MintMeBitch/static/js/main.ee9ecc32.chunk.js"
+  },
+  {
+    "revision": "fa78001ef51991d18282",
+    "url": "/theserdzo/2MintMeBitch/static/js/runtime-main.43548342.js"
+  },
+  {
+    "revision": "f34ad4b96032ac7b9a2835809acedb2b",
+    "url": "/theserdzo/2MintMeBitch/static/media/T.f34ad4b9.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/theserdzo/2MintMeBitch/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/theserdzo/2MintMeBitch/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/theserdzo/2MintMeBitch/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/theserdzo/2MintMeBitch/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/theserdzo/2MintMeBitch/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/theserdzo/2MintMeBitch/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/theserdzo/2MintMeBitch/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/theserdzo/2MintMeBitch/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/theserdzo/2MintMeBitch/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/theserdzo/2MintMeBitch/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "f34ad4b96032ac7b9a2835809acedb2b",
+    "url": "/theserdzo/2MintMeBitch/static/media/video-placeholder.f34ad4b9.jpg"
+  }
+]);
